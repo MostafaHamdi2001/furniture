9af21b8d560ef3fc06bbae3fd5a86422c9f637f9
MostafaHamdi2001 <158320404+MostafaHamdi2001@users.noreply.github.com>
2026-08-14 22:37:09 +0300

    update Blog In HeroSection

--- HeroSection.jsx
+++ HeroSection.jsx
@@ -9,14 +9,14 @@ function HeroSection() {
 					<div className="row justify-content-between">
 						<div className="col-lg-5">
 							<div className="intro-excerpt">
-								<h1>About Us</h1>
+								<h1>Blog</h1>
 								<p className="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.</p>
 								<p><a href="/shop" className="btn btn-secondary me-2">Shop Now</a><a href="#" className="btn btn-white-outline">Explore</a></p>
 							</div>
 						</div>
 						<div className="col-lg-7">
 							<div className="hero-img-wrap">
-								<img src="images/couch.png" className="img-fluid" />
+								<img src="images/couch.png" className="img-fluid" alt="Image" />
 							</div>
 						</div>
 					</div>
@@ -25,6 +25,7 @@ function HeroSection() {
 		{/* <!-- End Hero Section --> */}
 
         </div>
+
     )
 }
 
